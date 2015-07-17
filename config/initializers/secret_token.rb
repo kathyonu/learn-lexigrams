a2c87882aa03db64132834b5b4029a88605c838b
Will Schive <lexigram@goodworksonearth.org>
2015-07-17 09:39:13 -0700

    updated to 2.2.2 changed links

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-LearnLexigrams::Application.config.secret_key_base = 'c9e38fb228c7fb2abc4809ed057d78a08ce2fea18e00079fccb8b8bcd87c006927f836c67b6a8a52b6c629116c9d69edefa82d134b626651e992203b2b41264b'
+
+LearnLexigrams::Application.config.secret_key_base = ENV["SECRET_KEY_BASE"]
